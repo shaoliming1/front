77488f24384406c5a3d31b39120b8db40032f455
shaoliming <shaoliming@momenta.works>
2019-12-18 22:22:38 +0800

    abc

--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import iview from  'iview'
+import 'iview/dist/styles/iview.css';
 
 Vue.config.productionTip = false
+
 
 /* eslint-disable no-new */
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,10 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },{
+        path:'/login',
+        name:'login',
+        component:loginPage
     }
   ]
 })
